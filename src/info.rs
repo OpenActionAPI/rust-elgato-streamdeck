0b0d6b69b521b8d7bd43dddbcb81163e51b6951a
heyciao <14062695+heyciao@users.noreply.github.com>
2025-09-30 22:56:54 +0200

    Added functionality to 1) convert from "str" to "Kind"-enum and 2) from "Kind"-enum to "String".

--- src/info.rs
+++ src/info.rs
@@ -1,3 +1,5 @@
+use strum::{Display, EnumIter, EnumString};
+
 /// HIDAPI Vendor ID that Elgato products use
 pub const ELGATO_VENDOR_ID: u16 = 0x0fd9;
 
@@ -39,6 +41,7 @@ pub fn is_vendor_familiar(vendor: &u16) -> bool {
 
 /// Enum describing kinds of Stream Decks out there
 #[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
+#[derive(Display, EnumIter, EnumString)]
 pub enum Kind {
     /// First revision of original Stream Deck
     Original,
